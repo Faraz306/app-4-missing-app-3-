3a981530c59ff4901624ebe40454bd886e303416
Faraz <farazmoin.uk@gmail.com>
2025-11-28 20:17:56 +0530

    a PDF_MAKER topic2.

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ for index, row in df.iterrows():
 
     pdf.set_text_color(100, 100, 100)
 
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=0, border=0)
 
     pdf.line(10, 22, 200, 22)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("Variables.pdf")
